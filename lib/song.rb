db70be69c65dc2771a4e900706a03dd758fb0522
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-23 18:49:18 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @count = 0
+  @genres = []
+
+  def initialize
+    @count += 1 = count
+  end
+
+  def count
+    count
+  end
+
+  def genres
+    @genres << genre
+    @genres.uniq
+  end
+  
+
+
+end
