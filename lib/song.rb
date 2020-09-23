73774ac20904a4df4d29f047bb89de20d4007876
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-23 19:15:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,12 @@ class Song
 
   attr_accessor :name, :artist, :genre
 
-  @count = 0
+  #@count = 0
   @genres = []
   @artists = []
 
-  def initialize(name, artist, genre)
-    count = @count += 1
+  def initialize
+    #count = @count += 1
     @genre = genre
     @artist = artist
     @name = name
