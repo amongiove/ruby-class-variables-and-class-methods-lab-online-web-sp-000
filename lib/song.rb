a5c2bbb35b93bb91a0feec0595d6be389f0d9313
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-23 19:02:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,13 +28,27 @@ class Song
     genre_count = {}
     #for each genre in @genre, add key with value 1
     #if genre key already present, add +1 to value
-    @genre.each |key, value|
+    @genres.each |key, value|
       if genre_count.has_key? key
         value += 1
       else
         genre_count[key] => 1
       end
     end
+
+    def genre_count
+      genre_count = {}
+      #for each genre in @genre, add key with value 1
+      #if genre key already present, add +1 to value
+      @artists.each |key, value|
+        if artist_count.has_key? key
+          value += 1
+        else
+          artist_count[key] => 1
+        end
+      end
+
+
   end
 
 end
