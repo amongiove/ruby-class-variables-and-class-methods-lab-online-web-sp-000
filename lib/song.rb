2784e8c64e4e4d210461ed6a4176b6fb9666f722
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-23 19:18:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,15 +2,15 @@ class Song
 
   attr_accessor :name, :artist, :genre, :count
 
-  @count = 0
-  @genres = []
-  @artists = []
+  @@count = 0
+  @@genres = []
+  @@artists = []
 
   def initialize
-    count = @count += 1
-    @genre = genre
-    @artist = artist
-    @name = name
+    count = @@count += 1
+    @@genre = genre
+    @@artist = artist
+    @@name = name
   end
 
   def count
@@ -18,20 +18,20 @@ class Song
   end
 
   def genres
-    @genres << genre
-    @genres.uniq!
+    @@genres << genre
+    @@genres.uniq!
   end
 
   def artists
-    @artists << artist
-    @artists.uniq!
+    @@artists << artist
+    @@artists.uniq!
   end
 
   def genre_count
     genre_count = {}
     #for each genre in @genre, add key with value 1
     #if genre key already present, add +1 to value
-    @genres.each do |key, value|
+    @@genres.each do |key, value|
       if genre_count.has_key? key
         value += 1
       else
@@ -42,7 +42,7 @@ class Song
 
     def artist_count
       artist_count = {}
-      @artists.each do |key, value|
+      @@artists.each do |key, value|
         if artist_count.has_key? key
           value += 1
         else
